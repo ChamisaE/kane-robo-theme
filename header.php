41a0a8aa4d897014a381316e2264411d9b75ca8a
Chamisa Edmo <chamisaedmo@yahoo.com>
2019-04-02 16:35:42 -0600

    removed Kane Robotics from header

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php wp_head();?>
-    <title>Kane Robotics</title>
+<!--    <title>Kane Robotics</title> -->
 </head>
 
 <body>
