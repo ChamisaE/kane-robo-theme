6929e408c69dde3449e506e988a9797828dd2176
Chamisa Edmo <chamisaedmo@yahoo.com>
2019-04-02 17:25:30 -0600

    reordering the contact page

--- page-contact.php
+++ page-contact.php
@@ -1,15 +1,19 @@
 <?php get_header(); ?>
+	<div class="contact-container">
+		<div class="contact">
+
+			<p> <i class="fas fa-phone-square"></i>(562) 432-3276 </p>
+			<p> <i class="fas fa-address-card"></i> 101 Broadway Blvd NE Suite 3100 Albuquerque NM 87102 </p>
+			<p><i class="fas fa-envelope-square"></i>info@kaneaero.com</p>
+
+		</div>
+	</div>
+
+	<!--a fancy separator line-->
+	<hr class="my-4">
 
 <h2 class="section-heading" id="contact-us">Contact Us </h2>
     
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3264.8995989838268!2d-106.64871538482598!3d35.084241280337764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87220d816a9f8e89%3A0xe9927a14716e8ac2!2sBuild+With+Robots!5e0!3m2!1sen!2sus!4v1552506375070" width="1500" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe>
-<div class="contact-container">
-    <div class="contact">
-        
-        <p> <i class="fas fa-phone-square"></i>(562) 432-3276 </p>
-        <p> <i class="fas fa-address-card"></i> 101 Broadway Blvd NE Suite 3100 Albuquerque NM 87102 </p>
-        <p><i class="fas fa-envelope-square"></i>info@kaneaero.com</p>
 
-    </div>
-</div>               
 <?php get_footer(); ?>
