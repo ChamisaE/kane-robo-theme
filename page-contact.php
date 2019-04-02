7c92aa68a7f12e1c9a9efe21577805f6d3edf44a
Chamisa Edmo <chamisaedmo@yahoo.com>
2019-04-02 17:27:33 -0600

    oops rearranged again minu sfancy line

--- page-contact.php
+++ page-contact.php
@@ -1,4 +1,6 @@
 <?php get_header(); ?>
+
+<h2 class="section-heading" id="contact-us">Contact Us </h2>
 	<div class="contact-container">
 		<div class="contact">
 
@@ -8,12 +10,6 @@
 
 		</div>
 	</div>
-
-	<!--a fancy separator line-->
-	<hr class="my-4">
-
-<h2 class="section-heading" id="contact-us">Contact Us </h2>
-    
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3264.8995989838268!2d-106.64871538482598!3d35.084241280337764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87220d816a9f8e89%3A0xe9927a14716e8ac2!2sBuild+With+Robots!5e0!3m2!1sen!2sus!4v1552506375070" width="1500" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3264.8995989838268!2d-106.64871538482598!3d35.084241280337764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87220d816a9f8e89%3A0xe9927a14716e8ac2!2sBuild+With+Robots!5e0!3m2!1sen!2sus!4v1552506375070" width="1500" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe>
 
 <?php get_footer(); ?>
