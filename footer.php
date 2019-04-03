b1989868ffb89c5f53e1f6aa9419899d594f39a5
Chamisa Edmo <chamisaedmo@yahoo.com>
2019-04-03 15:00:55 -0600

    possibly hopefully maybe pushing the most upto date to newchamisa branch

--- footer.php
+++ footer.php
@@ -37,49 +37,7 @@
                 <p>(562) 432-3276 </p>
                 <p>101 Broadway Blvd NE Suite 3100 Albuquerque NM 87102 </p>
                 <p>info@kaneaero.com</p>
-<<<<<<< HEAD
-=======
-                <div id="social-media-footer">
-						 <ul>
-							 <li>
-								<a href="https://www.robotics.org/">
-									<img src="img/riaMember.jpeg" height="75" width="75">
-								</a>
-							 </li>
-								<li>
-									<a href="http://arminstitute.org/">
-										<img src="img/ARMMember.png" height="75" width="75">
-									</a>
-								</li>
-						 <li>
-							 <a href="https://www.universal-robots.com/">
-								 <img src="img/urobot.jpg" height="75" width="75">
-							 </a>
-						 </li>
-							 <!--<ul>
-												<li>
-													 <a href="#">
-														  <i class="fab fa-facebook"></i>
-													 </a>
-												</li>
-												<li>
-													 <a href="#">
-														  <i class="fab fa-youtube"></i>
-													 </a>
-												</li>
-												<li>
-													 <a href="#">
-														  <i class="fab fa-github"></i>
-													 </a>
-												</li>
-										  </ul>-->
-									 </div>
 
-
-
-        </footer>
-
-    
 >>>>>>> added universal robots logo, still not gonna format doe
 
                 <h3> Affiliations </h3>
