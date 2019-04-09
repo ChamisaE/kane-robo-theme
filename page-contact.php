b26c53b05b9a47a7b8d58f0ead314891b178b2db
Chamisa Edmo <chamisaedmo@yahoo.com>
2019-04-09 11:24:04 -0600

    reorder maps

--- page-contact.php
+++ page-contact.php
@@ -10,16 +10,19 @@
 
 		</div>
 	</div>
+
+	<!--California-->
+	<div id="california">
+		<h2>California</h2>
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5963.428044322492!2d-117.64210806310798!3d33.98176457770776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dccc9cd888dc83%3A0x5a10f1f97403cd03!2s7000+Merrill+Ave%2C+Chino%2C+CA+91710!5e0!3m2!1sen!2sus!4v1554824129749!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+
 	<!--New Mexico-->
 	<div id="newmexico">
 		<h2>New Mexico</h2>
 	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3264.8995989838268!2d-106.64871538482598!3d35.084241280337764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87220d816a9f8e89%3A0xe9927a14716e8ac2!2sBuild+With+Robots!5e0!3m2!1sen!2sus!4v1552506375070" width="1500" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe>
 	</div>
 
-	<!--California-->
-	<div id="california">
-		<h2>California</h2>
-	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5963.428044322492!2d-117.64210806310798!3d33.98176457770776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dccc9cd888dc83%3A0x5a10f1f97403cd03!2s7000+Merrill+Ave%2C+Chino%2C+CA+91710!5e0!3m2!1sen!2sus!4v1554824129749!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 
 	</div>
 
