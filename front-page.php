5b79e388438648ac5cbd35ef00eb490d9c4f41e8
dsanderson90 <dsanderson3@cnm.edu>
2019-04-12 10:35:40 -0600

    put products on top adding services next.

--- front-page.php
+++ front-page.php
@@ -6,27 +6,21 @@
     <div id="divider">
     </div>
     <main>
-        <a href="<?php echo site_url('/services');?>">
-            <h2 class="section-heading">Services</h2>
+        <a href="<?php echo site_url('/products');?>">
+            <h2 class="section-heading">Products</h2>
         </a>
 
-        <div class="event-section">
+           <?php
+            // query for the products page
+            $products = new WP_Query( 'pagename=products' );
+            // "loop" through query (even though it's just one page) 
+            while ( $products->have_posts() ) : $products->the_post(); ?>
+                <?php the_content();?>
+            <?php endwhile;
+            // reset post data (important!)
+            wp_reset_postdata();
+            ?>
 
-        <?php
-    // query for the services page
-    $services = new WP_Query( 'pagename=services' );
-    // "loop" through query (even though it's just one page) 
-    while ( $services->have_posts() ) : $services->the_post(); ?>
-    <div class="services-card">
-        <div class="card-description">
-            <?php the_content();?>
-        </div>
-    </div>
-
-    <?php endwhile;
-    // reset post data (important!)
-    wp_reset_postdata();
-?>
 
 
         </div>
