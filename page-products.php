d27d6720653e385f6495ab2f85bea899ea1c3780
dsanderson90 <dsanderson3@cnm.edu>
2019-04-16 16:12:12 -0600

    got products and services to display in mobile.

--- page-products.php
+++ page-products.php
@@ -18,4 +18,20 @@ get_header(); ?>
 <div class="rr rr-right">
 </div>
 
+<div class="mobile">
+
+<h2 class="section-heading">Products</h2>
+        <p>
+        <?php
+        if (have_posts()) :
+        while (have_posts()) :
+            the_post();
+                the_content();
+        endwhile;
+        endif;?>
+        </p>
+
+
+</div>
+
 <?php get_footer();  ?>
--- page-services.php
+++ page-services.php
@@ -16,8 +16,24 @@
         </p>
     </div>
 </div>
-<div class="rr rr-right">
+<div class="rr rr-right services">
 </div>
     
+<div class="mobile">
 
+<h2 class="section-heading">Services</h2>
+    <section>
+        <p>
+        <?php
+        if (have_posts()) :
+        while (have_posts()) :
+            the_post();
+                the_content();
+        endwhile;
+        endif;?>
+        </p>
+    </section>
+
+
+</div>
 <?php get_footer(); ?>
